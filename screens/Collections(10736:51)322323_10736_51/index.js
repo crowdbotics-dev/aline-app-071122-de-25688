8d10a860e91699ec0a2ba05968c2d03e1d9969a2
Crowdbotics <engineering@crowdbotics.com>
2022-07-12 18:45:28 +0000

    Update components

---
+++ screens/Collections(10736:51)322323_10736_51/index.js
@@ -0,0 +1,82 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_10736_52}>
+        <ImageBackground
+          source={{
+            uri:
+              "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/94bac275-78b8-4176-80f2-847d781f1543"
+          }}
+          style={styles.ImageBackground_10736_53}
+        />
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("111%") },
+  View_10736_52: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("99%"),
+    minHeight: hp("99%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    overflow: "hidden"
+  },
+  ImageBackground_10736_53: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("99%"),
+    minHeight: hp("99%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
